782070438edb37f8b8b871dba733a8a3772d0bc1
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-03-07 14:11:55 +0100

    f1-weather
    - Add footer and styled it;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
+import Footer from "./components/Footer"
 import MyNavBar from "./components/MyNavBar"
 
 function App() {
     return (
         <div className="App">
           <MyNavBar />
+          <Footer />
         </div>
     )
 }
