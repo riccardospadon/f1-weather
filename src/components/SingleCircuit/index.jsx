678677144e87e7ac9a8e312b853bb0fa5347939a
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-03-14 17:49:51 +0100

    f1-weather
    - Start "SingleCircuit";

--- src/components/SingleCircuit/index.jsx
+++ src/components/SingleCircuit/index.jsx
@@ -1,7 +1,25 @@
-import NavNoSearch from "../NavNoSearch";
+import { Container } from "react-bootstrap"
+import NavNoSearch from "../NavNoSearch"
+import styles from "./style.module.scss"
+import cn from "classnames"
+import { ArrowLeftShort, ArrowRightShort } from "react-bootstrap-icons"
 
-export default function SingleCircuit(){
-    return(
-        <NavNoSearch />
+export default function SingleCircuit() {
+    return (
+        <>
+            <NavNoSearch />
+            {/* Commands under the navbar for navigate from a circuit to the others */}
+            <Container className="d-flex justify-content-between">
+                <div>
+                    <ArrowLeftShort />
+                    Previous
+                </div>
+
+                <div>
+                    Next
+                    <ArrowRightShort />
+                </div>
+            </Container>
+        </>
     )
 }
