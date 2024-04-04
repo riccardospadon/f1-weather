9f4df50ad8e6268487e1b8c709b481117d358b19
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-04-04 17:51:19 +0200

    f1-weather
    - Add npm openmeteo;
    - Start writing objects for all circuits for openmeteo params;

--- src/components/SingleCircuit/index.jsx
+++ src/components/SingleCircuit/index.jsx
@@ -3,6 +3,10 @@ import styles from "./style.module.scss"
 import cn from "classnames"
 
 export default function SingleCircuit() {
+    const [circuit, setCircuit] = useState([])
+    const [openMeteoData, setOpenMeteoData] = useState({})
+
+    // tyres images
     const tyres = {
         C1: "http://d3nv2arudvw7ln.cloudfront.net/staticfolder/Tyre/resources/img/white-parentesi.png", // hard tyre
         C2: "http://d3nv2arudvw7ln.cloudfront.net/staticfolder/Tyre/resources/img/yellow-parentesi.png", // medium tyre
@@ -13,7 +17,44 @@ export default function SingleCircuit() {
             "https://d3nv2arudvw7ln.cloudfront.net/images/global/968/233/cinturato-blue-wet-4505508953865.png",
     }
 
+    // parameters for each location
+    const paramsLocation = {
+        Barhain: {
+            "latitude": 26.0348,
+            "longitude": 50.5245,
+            "start_date": "2024-02-29"
+        },
+
+        "Saudi Arabia": {
+            "latitude": 21.4901,
+            "longitude": 39.1862,
+            "start_date": "2024-02-29"
+        },
+
+
+    }
+
     // TODO: create a function that match the date from F1 API with the OPENMETEO ones...
+    
+    useEffect(() => {
+        // fetch f1 API
+        fetch(`https://api.openf1.org/v1/meetings?year=2024&circuit_key=${id}`, {})
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setCircuit(data)
+        })
+        .catch((error) => console.error("Error fetching circuit!", error))
+
+        // fetch openmeteo API
+        const fetchOpenMeteoData = async () =>{
+            
+        }
+    }, [])
+
+
+    
+
 
     // TODO: create a function that automatically see the location (maybe feel like into the main with flags and name countries) and return the right country and the weather too.
 
